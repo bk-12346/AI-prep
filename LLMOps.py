26fea529309a8b9662e2e9cb0cb303933009f01e
bk_ifti <127694899+bk-12346@users.noreply.github.com>
2025-12-12 20:43:17 +0400

    update

---
+++ LLMOps.py
@@ -0,0 +1,39 @@
+# LLMs are different from trasitional ML because:
+# 1. typically pre-trained
+# 2. have a huge number of parameters
+# 3. require significant computational resources
+# 4. can sometimes have unpredictable outcomes
+# 
+### LLMOps 
+# ensures the seamless integration of LLms into organizations
+# ensures smooth transition across lifecycle phases, from ideation to deployment
+# provide scalable, risk mitigating solutions
+
+##### Lifecycle of LLMs #####
+### 1. Ideation Phase
+## Data Sourcing: identifying data needs
+# is the data relevant?
+# is the data available? -> transform data, set up additional databases, evaluate costs, consider limitations
+# does the data meet standards?
+
+## Select base model selection
+# whether to use propiritry or open-source model
+# evaluate model on: 
+# 1. performance -> response quality + speed
+# 2. characteristics -> data used to train the model
+# 3. practical considerations -> type of liscense associated with the model
+# 4. important secondary factors -> popularity etc
+
+
+
+### 2. Development Phase
+# Prompt Engineering
+# Chains and Agents
+# RAG vs. fine-tuning
+# Testing
+
+### 3. Operational Phase
+# Deployment
+# Monitoring and Observability
+# Cost Management
+# Implementation of Governance and Security
